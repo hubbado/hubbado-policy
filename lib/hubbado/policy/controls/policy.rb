9c28048c9ddb54d38263d7d38a97486e277a9837
Alfonso Uceda <alfonso@hubbado.com>
2025-05-26 17:05:42 +0200

    Policy control is improved:
    When using a policy control you could permit or deny with a method
        ```ruby
        mimic_policy = Control::Policies::ArticlePolicy.example
        mimic_policy.permit(:view)
        # or
        mimic_policy.deny(:view)
        mimic_policy.deny(:view, :a_reason) # with a reason
        mimic_policy.deny(:view, data: :custom_data) # with data
        mimic_policy.deny(:view, :a_reason, data: :custom_data) # or both
        ```

--- lib/hubbado/policy/controls/policy.rb
+++ lib/hubbado/policy/controls/policy.rb
@@ -2,6 +2,8 @@ module Hubbado
   module Policy
     module Controls
       class Policy
+        UnkownPolicy = Class.new(StandardError)
+
         def self.mimic(policy_class)
           policy_class.policies.each do |policy|
             send(:attr_writer, policy)
@@ -13,6 +15,22 @@ module Hubbado
             define_method "#{policy}?" do
               send(policy).permitted?
             end
+          end
+
+          define_method :permit do |policy|
+            unless policy_class.instance_variable_get("@policies").include?(policy)
+              raise UnkownPolicy
+            end
+
+            send("#{policy}=", policy_class.permitted)
+          end
+
+          define_method :deny do |policy, reason = nil, data: nil|
+            unless policy_class.instance_variable_get("@policies").include?(policy)
+              raise UnkownPolicy
+            end
+
+            send("#{policy}=", policy_class.denied(reason, data: data))
           end
         end
 
