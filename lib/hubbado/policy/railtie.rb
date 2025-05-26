2f7475b35164bbe1310659be1c661149ec25a2ef
Alfonso Uceda <alfonso@hubbado.com>
2025-05-26 10:09:08 +0200

    Railtie is corrected with an initializer

--- lib/hubbado/policy/railtie.rb
+++ lib/hubbado/policy/railtie.rb
@@ -1,7 +1,9 @@
 module Hubbado
   module Policy
     class Railtie < ::Rails::Railtie
-      ::I18n.load_path << ::File.expand_path("../../../../config/locales/en.yml", __FILE__)
+      initializer "hubbado-policy.initialized" do |app|
+        ::I18n.load_path << ::File.expand_path("../../../../config/locales/en.yml", __FILE__)
+      end
     end
   end
 end
